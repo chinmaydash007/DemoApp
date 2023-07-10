38025a3d0d139ac3040211ead8a83e4f293b9ac0
chinmay <chinmaydash11@gmail.com>
2023-07-10 12:30:47 +0530

    added camera plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,39 @@ export class HomePage {
 
   counter:number=0
 
-  constructor() {}
+  clickedImage: string="";
+
+  options: CameraOptions = {
+    quality: 30,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+
+  constructor(private camera: Camera) {}
 
   descriptionText="This is random Description" 
 
   increaseCounter(){
     this.counter=this.counter+1
   }
+
+
+  captureImage() {
+
+
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.clickedImage = base64Image;
+    }, (err) => {
+      console.log(err);
+      // Handle error
+      alert(err)
+    });
+  }
+
 
 }
